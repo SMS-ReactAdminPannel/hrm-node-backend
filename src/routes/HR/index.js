fcb132a88608c23083d1545553f6818aced3b8c5
Prakash-yoho <prakash.yohotechnologies@gmail.com>
2025-06-12 12:42:06 +0530

    deduction of api backend

--- src/routes/HR/index.js
+++ src/routes/HR/index.js
@@ -2,10 +2,12 @@ import express from "express"
 import LeaveRouter from "./LeaveManagement/index.js";
 import LeaveTypeRouter from "./LeaveTypeManagement/index.js";
 import ShiftRouter from "./Shift/index.js";
+import DeductionRouter from "./Deduction/index.js";
 
 const HrRouter = express.Router();
 
 HrRouter.use("/leave", LeaveRouter)
 HrRouter.use("/leave-type", LeaveTypeRouter)
 HrRouter.use("/shift",ShiftRouter)
+HrRouter.use("/deductions",DeductionRouter)
 export default HrRouter;
